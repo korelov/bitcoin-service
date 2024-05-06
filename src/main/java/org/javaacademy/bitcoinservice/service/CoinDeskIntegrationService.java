dc19c1b353cc780240e7c9e3380c09d40383f109
Korelov Maksim <korelov@gmail.com>
2024-05-06 23:59:21 +0300

    done

--- src/main/java/org/javaacademy/bitcoinservice/service/CoinDeskIntegrationService.java
+++ src/main/java/org/javaacademy/bitcoinservice/service/CoinDeskIntegrationService.java
@@ -1,12 +1,19 @@
 package org.javaacademy.bitcoinservice.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.javaacademy.bitcoinservice.config.CoinDeskProperties;
 import org.javaacademy.bitcoinservice.dto.CoindeskDtoRs;
 import org.javaacademy.bitcoinservice.exception.IntegrationException;
 import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +21,24 @@ public class CoinDeskIntegrationService {
     private final RestTemplate restTemplate;
     private final CoinDeskProperties properties;
 
-    public CoindeskDtoRs getCoindeskDtoRs() {
+    /**
+     * @return
+     * два варианта получения стоимости биткоина в USD
+     * через ObjectMapper
+     * и через Dto
+     * ObjectMapper можно добавить в конфин в виде бина !!
+     */
+    @SneakyThrows
+    public CoindeskDtoRs getCoindeskDtoRs()  {
+        ResponseEntity<String> response = restTemplate.getForEntity(properties.getBaseUrl(), String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        BigDecimal price = mapper.readTree(response.
+                getBody()).
+                path("bpi").
+                path("USD").
+                path("rate_float").
+                decimalValue();
+
         RequestEntity<Void> request = RequestEntity
                 .get(properties.getBaseUrl())
                 .build();
